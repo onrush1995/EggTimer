227f310461fdd758c5677ea400aaec2ec58e1292
onrush1995 <moniruzzaman.eng1995@gmail.com>
2019-05-01 18:20:38 +0300

    SeekBar value pass to textView

--- app/src/main/java/demo/gestures/eggtimeer/MainActivity.java
+++ app/src/main/java/demo/gestures/eggtimeer/MainActivity.java
@@ -2,6 +2,8 @@ package demo.gestures.eggtimeer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar=findViewById(R.id.timerSeekBar);
+        final TextView timerTextView=findViewById(R.id.countdownTextView);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int minutes=progress/60;
+                int seconds=progress-(minutes*60);
+
+                timerTextView.setText(Integer.toString(minutes)+":"+Integer.toString(seconds));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
